1aed66f68e5b656b73fc9caaef9edb466589af48
ShibiSalu <shibisalu796@gmail.com>
2025-06-20 14:35:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bed-2-scn-1",
+      "name": "BED 2 SCN 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.06536916669970338,
+        "pitch": -0.028479220141431227,
+        "fov": 1.213828912529103
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BEDROOM 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
